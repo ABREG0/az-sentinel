30c243c71371b1a49da4e87f2fff9072f3d5e50f
elsalvos <74327636+CABREG0@users.noreply.github.co>
2021-04-16 08:06:08 -0700

    initial

---
+++ import-rules/Do-Checks.ps1
@@ -0,0 +1,95 @@
+
+$PSrequiredVersion = 6.2
+
+write-host "`nCheck if PS was launched as admin..." -ForegroundColor Yellow
+if (-NOT ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator"))  
+{  
+  
+  $arguments = $SCRIPT:MyInvocation.MyCommand.Path #"& '" +$myinvocation.mycommand.definition + "'"
+
+  write-host "`ncheck if PS was launched as admin... re-launching the script: `n $($arguments) " -ForegroundColor Red
+
+  Start-Process powershell -Verb runAs -ArgumentList $arguments
+  
+  Break
+} 
+ else{
+    Write-Host "`nPowerShell was Launched as Admin... " -ForegroundColor Green
+ }
+
+ Function Install-PowerShell7 {
+$TLS12Protocol = [System.Net.SecurityProtocolType] 'Ssl3 , Tls12'
+[System.Net.ServicePointManager]::SecurityProtocol = $TLS12Protocol
+
+   
+   $sourceURL = 'https://github.com/PowerShell/PowerShell/releases/download/v7.1.3/PowerShell-7.1.3-win-x64.msi'
+
+   $destination = "$env:TEMP\PowerShell-7.1.3-win-x64.msi"
+   
+   Invoke-webrequest -uri $sourceURL -outfile $destination
+   
+   $InstallArg = "/package $($destination) /qb ADD_EXPLORER_CONTEXT_MENU_OPENPOWERSHELL=1 ENABLE_PSREMOTING=1 REGISTER_MANIFEST=1"
+   
+   Start-Process -FilePath msiexec -ArgumentList $InstallArg -wait # '/x C:\temp\PowerShell-7.1.3-win-x64.msi /uninstall /qf' -Wait
+   #msiexec.exe /package $destination /qb ADD_EXPLORER_CONTEXT_MENU_OPENPOWERSHELL=1 ENABLE_PSREMOTING=1 REGISTER_MANIFEST=1
+
+   Remove-Item $destination -Force
+
+}
+
+ Function Test-PSVersion{
+   param(
+      $PsVersion
+   )
+   $PSrequiredVersion = $PsVersion
+   $PSInstalled = Get-ItemProperty HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\* | Where-Object {$_.DisplayName -match "PowerShell [0-9]-x" } 
+   
+   if($null -ne $PSInstalled){
+
+       Write-Host "`n PS core is installed... Checking exact version"
+       
+       If($PSInstalled.DisplayVersion -ge $PSrequiredVersion){
+   
+           Write-Host "`n PS core version is greater than $($PSrequiredVersion) required for AzSentinel Module"
+   
+           $PSInstalled.DisplayVersion
+       }
+       return $true 
+   } 
+    else{
+       Write-Host "Downloading and Installing PowerShell Core latest version"
+         Return $false 
+         #Install-PowerShell7
+    }
+   
+}
+
+if(Test-PSVersion -PsVersion 6.2){
+   Write-Host "PS Core is installed" -ForegroundColor Green
+}
+ else{
+    Write-Host "Installing PS Core v7" -ForegroundColor Red
+    Install-PowerShell7
+ }
+
+Write-Host "`nStarting PS version" -ForegroundColor Red
+$PSVersionTable.PSVersion
+
+if ($PSVersionTable.PSVersion -lt $PSrequiredVersion)
+{
+    Write-Host "`nScript was launch from PS ver below $($PSrequiredVersion)... re-launching the script with ps7. `n $($SCRIPT:MyInvocation.MyCommand.Path)" -ForegroundColor Cyan 
+    
+	pwsh -f $SCRIPT:MyInvocation.MyCommand.Path
+
+	return
+}
+ else{
+
+    Write-Host "`nScript was launch from PS vps7" -ForegroundColor Cyan 
+ }
+
+
+& .\Import-azSentinelRules.ps1
+
+Write-Host "`nSleeping for 20 seconds before exiting new admin PS window..."
+Start-Sleep -Seconds 20
---
+++ import-rules/import-azSentinelRules.ps1
@@ -0,0 +1,83 @@
+
+write-host "PS version $($psversionTable.psversion)" -ForegroundColor red
+    
+#check for Azure az module
+if($null -eq (Get-InstalledModule -Name az)){
+write-host "Installing Azure az module"
+Install-Module -Name Az -Scope CurrentUser -Repository PSGallery -Force -AllowClobber
+}
+
+#check for Azure az module
+if($null -eq (Get-InstalledModule -Name AzSentinel)){
+write-host "Installing Azure AzSentinel module"
+Install-Module -Name AzSentinel -Scope CurrentUser -Repository PSGallery -Force -AllowClobber
+}
+
+#disconnect exiting connections and clearing contexts.
+Write-Output "Clearing existing Azure connection `n"
+    
+Disconnect-AzAccount -ContextName 'MyContext' | Out-Null
+    
+Write-Output "Clearing existing Azure context `n"
+    
+get-azcontext -ListAvailable | ForEach-Object{$_ | remove-azcontext -Force -Verbose | Out-Null} #remove all connected content
+    
+Write-Output "`nClearing of existing connection and context completed. `n"
+Try{
+    #Connect to tenant with context name and save it to variable
+    $ConnectToTentant = Connect-AzAccount -Tenant '' -ContextName 'MyContext' -Force -ErrorAction Stop
+        
+    #Select subscription to build
+    $GetSubscriptions = Get-AzSubscription | Where-Object {($_.state -eq 'enabled') } | Out-GridView -Title "Select Subscription to build" -PassThru 
+        
+    }
+    catch{
+        
+    Write-Error "Error When trying to connct to tenant...`n tenant ID in this link: https://portal.azure.com/#blade/Microsoft_AAD_IAM/ActiveDirectoryMenuBlade/Overview"
+        
+    $_ ;
+        
+    Return 
+        
+    }
+#loop through each selected subscription.. 
+foreach($GetSubscription in $GetSubscriptions)
+{
+
+Try{
+	#Set context for subscription being built
+	$SubContext = Set-AzContext -Subscription $GetSubscription.id
+
+    Write-Host "`nWorking in Subscription: $($GetSubscription.Name)"
+
+    $LAW = get-AzOperationalInsightsWorkspace 
+    if($null -eq $LAW){
+        Write-Host "No Log Analytics workspace found..." -ForegroundColor red 
+    }
+    else{
+        Write-Host "`nListing Log Analytics workspace" -ForegroundColor Green
+        $LAW
+            
+        Write-host "Importing Azure Sentinel Rules" -ForegroundColor Green
+        Import-AzSentinelAlertRule -WorkspaceName "law-sentinel" -SettingsFile .\rules.json
+    }
+
+    $ResourceGroups = Get-AzResourceGroup
+    if($null -eq $ResourceGroups){
+        Write-Host "No Resource Group found..." -ForegroundColor red 
+    }
+        else{
+        Write-Host "`nListing Resource Groups" -ForegroundColor Green
+        $ResourceGroups.ResourceGroupName
+        }
+		 	}
+	catch [Exception]{ 
+
+		Write-warning "Error Message: `n$_ "
+
+		$_
+
+		}
+		 
+} 
+
